975ce7c7798139a86cfee8f1288b705659a756f5
Rickard Andersson <gonz@severnatazvezda.com>
2020-07-01 21:56:23 +0300

    add `characteristics` destructuring

--- src/coff.zig
+++ src/coff.zig
@@ -23,7 +23,7 @@ pub const COFFHeader = struct {
     symbols: u32,
     optional_header_size: u16,
     // @TODO: add deconstruction of this flags value
-    characteristics: u16,
+    characteristics: Characteristics,
 };
 
 const x64_tag = "\x64\x86";
@@ -71,7 +71,41 @@ pub fn readCOFFHeader(file: fs.File, pe_buffer: []u8) !COFFHeader {
     const optional_header_size = mem.bytesToValue(u16, optional_header_size_bytes);
 
     const characteristics_bytes = pe_buffer[22..24];
-    const characteristics = mem.bytesToValue(u16, characteristics_bytes);
+    const cv = mem.bytesToValue(u16, characteristics_bytes);
+
+    const relocations_stripped = ((cv & relocations_stripped_position) >> 0) == 1;
+    const executable_image = ((cv & executable_image_position) >> 1) == 1;
+    const line_numbers_stripped = ((cv & line_numbers_stripped_position) >> 2) == 1;
+    const local_symbols_stripped = ((cv & local_symbols_stripped_position) >> 3) == 1;
+    const working_set_trim = ((cv & line_numbers_stripped_position) >> 4) == 1;
+    const large_address_aware = ((cv & large_address_aware_position) >> 5) == 1;
+    const little_endian = ((cv & little_endian_position) >> 7) == 1;
+    const machine_32bit = ((cv & machine_32bit_position) >> 8) == 1;
+    const debugging_info_stripped = ((cv & debugging_info_stripped_position) >> 9) == 1;
+    const removable_run_from_swap = ((cv & removable_run_from_swap_position) >> 10) == 1;
+    const net_run_from_swap = ((cv & net_run_from_swap_position) >> 11) == 1;
+    const file_system = ((cv & file_system_position) >> 12) == 1;
+    const dll = ((cv & dll_position) >> 13) == 1;
+    const uniprocessor_only = ((cv & uniprocessor_only_position) >> 14) == 1;
+    const big_endian = ((cv & big_endian_position) >> 15) == 1;
+
+    const characteristics = Characteristics{
+        .relocations_stripped = relocations_stripped,
+        .executable_image = executable_image,
+        .line_numbers_stripped = line_numbers_stripped,
+        .local_symbols_stripped = local_symbols_stripped,
+        .working_set_trim = working_set_trim,
+        .large_address_aware = large_address_aware,
+        .little_endian = little_endian,
+        .machine_32bit = machine_32bit,
+        .debugging_info_stripped = debugging_info_stripped,
+        .removable_run_from_swap = removable_run_from_swap,
+        .net_run_from_swap = net_run_from_swap,
+        .file_system = file_system,
+        .dll = dll,
+        .uniprocessor_only = uniprocessor_only,
+        .big_endian = big_endian,
+    };
 
     return COFFHeader{
         .machine_type = machine_type,
@@ -83,3 +117,37 @@ pub fn readCOFFHeader(file: fs.File, pe_buffer: []u8) !COFFHeader {
         .characteristics = characteristics,
     };
 }
+
+const relocations_stripped_position = 0x0001;
+const executable_image_position = 0x0002;
+const line_numbers_stripped_position = 0x0004;
+const local_symbols_stripped_position = 0x0008;
+const working_set_trim_position = 0x0010;
+const large_address_aware_position = 0x0020;
+const little_endian_position = 0x0080;
+const machine_32bit_position = 0x0100;
+const debugging_info_stripped_position = 0x0200;
+const removable_run_from_swap_position = 0x0400;
+const net_run_from_swap_position = 0x0800;
+const file_system_position = 0x1000;
+const dll_position = 0x2000;
+const uniprocessor_only_position = 0x4000;
+const big_endian_position = 0x8000;
+
+pub const Characteristics = struct {
+    relocations_stripped: bool,
+    executable_image: bool,
+    line_numbers_stripped: bool,
+    local_symbols_stripped: bool,
+    working_set_trim: bool,
+    large_address_aware: bool,
+    little_endian: bool,
+    machine_32bit: bool,
+    debugging_info_stripped: bool,
+    removable_run_from_swap: bool,
+    net_run_from_swap: bool,
+    file_system: bool,
+    dll: bool,
+    uniprocessor_only: bool,
+    big_endian: bool,
+};
